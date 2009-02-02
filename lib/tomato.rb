001f68d05d02a5a8971c8d9b373215cd9c0608ba
reborg <reborg@reborg.net>
2009-02-02 10:18:49 -0600

    Forgot to force a global commit also of deleted files.

--- lib/tomato.rb
+++
@@ -1,8 +0,0 @@
-class Tomato
-  attr_accessor :text
-  
-  def initialize(params = {})
-    @text = params[:text]
-  end
-  
-end
--- lib/tomatoes_controller.rb
+++
@@ -1,9 +0,0 @@
-class TomatoesController
-  
-  def create(params)
-    tomato = Tomato.new(params)
-    storage = Storage.new
-    storage.save(tomato)
-  end
-  
-end
