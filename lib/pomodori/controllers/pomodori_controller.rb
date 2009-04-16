abe5af61645a8de994260ab060d19015f14aa5de
reborg <reborg@reborg.net>
2009-04-15 19:23:32 -0500

    Just comments on controller pomodori

--- lib/pomodori/controllers/pomodori_controller.rb
+++ lib/pomodori/controllers/pomodori_controller.rb
@@ -2,23 +2,40 @@ require 'pomodori/views/main_view'
 require 'pomodori/kirby_storage'
 require 'pomodori/models/pomodoro'
 
+##
+# Pomodoro related operations triggered by
+# the UI
+#
 class PomodoriController
   attr_accessor :storage
   attr_accessor :main_view
 
+  ##
+  # Controllers must be initialized with related views.
+  #
   def initialize(params = {})
     @main_view = params[:main_view]
   end
   
+  ##
+  # Creates a new Pomodoro
+  #
   def create(params)
     pomodoro = Pomodoro.new(params)
     storage.save(pomodoro)
   end
 
+  ##
+  # Returns the count of Pomodoros stored on
+  # yesterday.
+  #
   def yesterday_pomodoros
     storage.find_all_day_before(Pomodoro, Time.now).size
   end
   
+  ##
+  # Returns the count of Pomodoros stored today.
+  #
   def today_pomodoros
     storage.find_all_by_date(Pomodoro, Time.now).size
   end
@@ -28,7 +45,8 @@ class PomodoriController
   end
   
   ##
-  # Calculates the daily average pomodoros
+  # Calculates the daily average pomodoros. The rounding
+  # is not really important here.
   #
   def daily_average
     all = PomodoroCountByDay.find_all
