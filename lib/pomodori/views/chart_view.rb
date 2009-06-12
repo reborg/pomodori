b211331643a0a4a74c40d2c201e373c3ed3090d9
reborg <reborg@reborg.net>
2009-06-12 16:53:36 -0500

    Conforming to view development guidelines

--- lib/pomodori/views/chart_view.rb
+++ lib/pomodori/views/chart_view.rb
@@ -7,77 +7,72 @@ require 'pomodori/controllers/charts_controller'
 #
 class ChartView
   include HotCocoa
-  attr_reader :top_view, :bottom_view, :reload_button, :close_button, :browser, :chart_window
+  attr_reader :hc_top_view, :hc_bottom_view, :hc_reload_button
+  attr_reader :hc_close_button, :hc_web_view, :hc_chart_window
 
   def initialize(opts = {})
     @charts_controller = opts[:charts_controller]
   end
   
-  def render
-    chart_window
-  end
-  
-  def chart_window
-    @chart_window ||= window(:frame => [100, 100, 500, 300], :title => "Reports") do |win|
-      win << bottom_view
-      win << top_view
-      @charts_controller.on_load_view
-    end
-  end
-  
-  def top_view
-    @top_view ||= layout_view(
-      :mode => :horizontal,
-      :layout => {:expand => [:width, :height]},
-      :margin => 0, :spacing => 0) do |view|
-      view << browser
-    end
-  end
-  
-  def bottom_view
-    @bottom_view ||= layout_view(
-      :mode => :horizontal,      
-      :frame => [0, 0, 450, 50]) do |view|
-      view << reload_button
-      view << close_button      
-    end
-  end
-  
-  def reload_button
-    @reload_button ||= button(
-        :title => "Reload",
-        :bezel => :textured_square,
-        :frame => [525, 12, 66, 28],
-        :on_action => reload_button_action)
-  end
-  
-  ##
-  # FIXME: extracted for testing because:
-  # ChartView.reload_button.on_action returns an NSButton
-  # and not a proc.
-  # 
-  def reload_button_action
-    lambda {|sender| @charts_controller.on_reload_chart}
-  end
-  
-  def close_button
-    @close_button ||= button(
-        :title => "Close",
-        :bezel => :textured_square,
-        :frame => [600, 12, 66, 28],
-        :on_action => Proc.new {@chart_window.close})
-  end
-  
-  def browser
-    @browser ||= web_view(
-      :layout => {:expand =>  [:width, :height]})
-  end
-  
-  ##
-  # SERVICES
-  ##
-  
   def load_chart(url)
-    browser.url = url
+    hc_web_view.url = url
   end
+  
+  def render
+    hc_chart_window
+  end
+ 
+  private
+
+    def hc_chart_window
+      @hc_chart_window ||= window(:frame => [100, 100, 500, 300], :title => "Reports") do |win|
+        win << hc_bottom_view
+        win << hc_top_view
+        @charts_controller.on_load_view
+      end
+    end
+    
+    def hc_top_view
+      @hc_top_view ||= layout_view(
+        :mode => :horizontal,
+        :layout => {:expand => [:width, :height]},
+        :margin => 0, :spacing => 0) do |view|
+        view << hc_web_view
+      end
+    end
+    
+    def hc_bottom_view
+      @hc_bottom_view ||= layout_view(
+        :mode => :horizontal,      
+        :frame => [0, 0, 450, 50]) do |view|
+        view << hc_reload_button
+        view << hc_close_button      
+      end
+    end
+    
+    def hc_reload_button
+      @hc_reload_button ||= button(
+          :title => "Reload",
+          :bezel => :textured_square,
+          :frame => [525, 12, 66, 28],
+          :on_action => reload_button_action)
+    end
+    
+    def reload_button_action
+      lambda {|sender| @charts_controller.on_reload_chart}
+    end
+    
+    def hc_close_button
+      @hc_close_button ||= button(
+          :title => "Close",
+          :bezel => :textured_square,
+          :frame => [600, 12, 66, 28],
+          :on_action => Proc.new {@hc_chart_window.close})
+    end
+    
+    def hc_web_view
+      @hc_web_view ||= web_view(
+        :layout => {:expand =>  [:width, :height]})
+    end
+
 end
--- test/pomodori/views/chart_view_test.rb
+++ test/pomodori/views/chart_view_test.rb
@@ -10,22 +10,22 @@ class ChartViewTest < Test::Unit::TestCase
   end
   
   def test_should_load_url
-    @chart_view.browser.expects(:url=).with("/temp/some.html")
+    @chart_view.send(:hc_web_view).expects(:url=).with("/temp/some.html")
     @chart_view.load_chart("/temp/some.html")
   end
   
   def test_should_add_webview_on_top
-    assert_equal(1, @chart_view.top_view.subviews.size)
-    assert_kind_of(WebView, @chart_view.top_view.subviews[0])
+    assert_equal(1, @chart_view.send(:hc_top_view).subviews.size)
+    assert_kind_of(WebView, @chart_view.send(:hc_top_view).subviews[0])
   end
   
   def test_should_add_2_buttons_bottom
-    assert_equal(2, @chart_view.bottom_view.subviews.size)
+    assert_equal(2, @chart_view.send(:hc_bottom_view).subviews.size)
   end
   
   def test_delegates_to_ctrl_on_reload
     @charts_controller.expects(:on_reload_chart)
-    @chart_view.reload_button_action.call("sender")
+    @chart_view.send(:reload_button_action).call("sender")
   end
   
 end
