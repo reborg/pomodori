c5ae6d9c14d923e32cc3588d9dfcd55b0b8a2f74
reborg <reborg@reborg.net>
2009-12-26 17:35:00 +0100

    project cleanup: moved classes in their subdirs

--- Tests/run_suite.rb
+++ Tests/run_suite.rb
@@ -1,2 +1,1 @@
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 Dir.glob(File.expand_path('../**/*_test.rb', __FILE__)).each { |test| require test }
--- Tests/test_helper.rb
+++ Tests/test_helper.rb
@@ -1,3 +1,4 @@
+require File.expand_path(File.dirname(__FILE__) + "/../Config/environment")
 require 'mocha'
 require 'matchy'
 require 'test/unit'
